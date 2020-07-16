c3935f03e622f01ee7e5ddbdb44036af4a1b8900
Stephen Kennedy <stephenkennedy@stephens-pro.hitronhub.home>
2020-07-16 14:26:53 -0400

    Initial Commit

---
+++ StopThiefDataTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopThiefDataTest
+//
+//  Created by Stephen Kennedy on 2020-07-16.
+//  Copyright © 2020 Stephen Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
